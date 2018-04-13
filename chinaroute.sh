667da2b5acbd7e77d495245272c0aade075ec48a
CNMan <nklijl@mail.nankai.edu.cn>
2018-04-13 00:02:37 +0000

    Fri Apr 13 00:02:37 UTC 2018

--- chinaroute.sh
+++ chinaroute.sh
@@ -3,7 +3,7 @@ wget -N http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-extended-latest
 wget -N http://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest
 wget -N http://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest
 wget -N http://ftp.ripe.net/pub/stats/ripencc/delegated-ripencc-extended-latest
-cat delegated-afrinic-extended-latest delegated-apnic-extended-latest delegated-arin-extended-latest delegated-lacnic-extended-latest delegated-ripencc-extended-latest>>delegated-all-latest
+cat delegated-afrinic-extended-latest delegated-apnic-extended-latest delegated-arin-extended-latest delegated-lacnic-extended-latest delegated-ripencc-extended-latest >delegated-all-latest
 cat delegated-all-latest | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' >cnroute.txt
 cat delegated-all-latest | grep ipv4 | grep HK | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' >hkroute.txt
 cat delegated-all-latest | grep ipv4 | grep MO | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' >moroute.txt
